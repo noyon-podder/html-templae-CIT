adb389d65c5e03d3a788a112498920d7c7ebd21e
noyon-podder <noyon.podder7@gmail.com>
2024-02-24 18:56:47 +0600

    education update site add this commit

---
+++ js/app.js
@@ -0,0 +1,188 @@
+
+
+// alert("hello")
+
+
+
+$('.testimonial-active').slick({
+    infinite: true,
+    speed: 1000,
+    centerMode: true,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 4,
+    autoplaySpeed: 4000,  
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+            centerMode: false,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: false,
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          centerMode: false,
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  
+  
+  //fixed navbar function
+  let header = document.getElementById("header");
+
+  window.addEventListener("scroll", function(){
+
+    if(document.body.scrollTop > 350 || document.documentElement.scrollTop > 350 ){
+      header.classList.add("header-fixed")
+    }else{
+      header.classList.remove("header-fixed")
+    }
+  })
+
+
+
+// scroll to top button 
+document.addEventListener("DOMContentLoaded", function() {
+
+    let scrollTOTopBtn = document.querySelector('.scroll-top')
+  
+    window.addEventListener("scroll", function() {
+      // Show or hide the button based on the scroll position
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollTOTopBtn.classList.add("open");
+      } else {
+        scrollTOTopBtn.classList.remove("open");
+      }
+    });
+  
+    scrollToTopBtn.addEventListener("click", function() {
+      // Scroll to the top with smooth animation
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    });
+  });
+  
+
+
+  // course details page tab 
+  
+  document.addEventListener("DOMContentLoaded", function () {
+
+    var defaultTabId = "tab1";
+    document.getElementById(defaultTabId).style.display = "block";
+    document
+      .querySelector("[onclick=\"openTab('" + defaultTabId + "')\"]")
+      .classList.add("active");
+  });
+
+  function openTab(tabId) {
+
+    var tabContents = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < tabContents.length; i++) {
+      tabContents[i].style.display = "none";
+    }
+
+
+    var tabs = document.getElementsByClassName("tab");
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].classList.remove("active");
+    }
+
+
+    document.getElementById(tabId).style.display = "block";
+
+
+    document
+      .querySelector("[onclick=\"openTab('" + tabId + "')\"]")
+      .classList.add("active");
+  }
+
+
+// gallery page slider
+
+$('.gallery-slider').slick({
+  infinite: true,
+  speed: 1000,
+  centerMode: false,
+  autoplay: true,
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  autoplaySpeed: 4000,  
+  // prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+  // nextArrow: '<button type="button" class="slick-next">Next</button>',
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+          centerMode: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: false,
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+
+// video modal open when click the icon
+
+  // JavaScript code to handle modal opening and closing
+  document.getElementById('openVideoBtn').addEventListener('click', function() {
+    document.getElementById('videoModal').classList.remove('hidden');
+  });
+
+  document.getElementById('closeVideoBtn').addEventListener('click', function() {
+    document.getElementById('videoModal').classList.add('hidden');
+  });
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          // main: '#25cdc9',
+          main: '#00A652',
+          // primary:"#fee823",
+          primary:"#8A171C",
+          white: '#fff',
+          heading: "#202020",
+          border: "#f1f1f1",
+          black: "#414141"
+        },
+        container: {
+            center: true,
+            padding: {
+              DEFAULT: "1rem",
+              md: "2rem",
+              lg: "3rem",
+              xl: "4rem",
+              "2xl": "4rem"
+            },
+          },
+      }
+    }
+  }
